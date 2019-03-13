6c7375a64c363eab6ecc565c548eea84521a082e
Artyom Efimov <artyomefimov97@gmail.com>
2019-03-13 16:44:31 +0400

    implemented recording in a hardcoded file

--- app/src/main/java/com/artyomefimov/soundrecorder/MainActivity.kt
+++ app/src/main/java/com/artyomefimov/soundrecorder/MainActivity.kt
@@ -1,12 +1,99 @@
 package com.artyomefimov.soundrecorder
 
-import android.support.v7.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaRecorder
 import android.os.Bundle
+import android.os.Environment
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
+
+    private var output: String? = null
+    private var mediaRecorder: MediaRecorder? = null
+    private var isRecording: Boolean = false
+    private var isPermissionGranted: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        if (isPermissionNotGranted(Manifest.permission.RECORD_AUDIO)
+            && isPermissionNotGranted(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+        ) {
+
+            val permissions = arrayOf(
+                android.Manifest.permission.RECORD_AUDIO,
+                android.Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                android.Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+
+            ActivityCompat.requestPermissions(this, permissions, 10)
+        } else {
+            isPermissionGranted = true
+        }
+
+        output =
+            Environment.getExternalStorageDirectory().absolutePath + "/record.mp3"
+
+        button_start.setOnClickListener {
+            if (isPermissionGranted) {
+                startRecording()
+            }
+        }
+
+        button_stop.setOnClickListener {
+            stopRecording()
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == 10 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            isPermissionGranted = true
+        }
+    }
+
+    private fun isPermissionNotGranted(permission: String): Boolean {
+        return ContextCompat.checkSelfPermission(this, permission) != PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startRecording() {
+        try {
+            resetMediaRecorder()
+            mediaRecorder?.prepare()
+            mediaRecorder?.start()
+            isRecording = true
+
+            toast("Recording was started")
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun stopRecording() {
+        if (isRecording) {
+            mediaRecorder?.stop()
+            mediaRecorder?.release()
+            mediaRecorder = null
+            isRecording = false
+
+            toast("Recording was stopped")
+        } else {
+            toast("Recording is not active")
+        }
+    }
+
+    private fun resetMediaRecorder() {
+        mediaRecorder = MediaRecorder()
+        mediaRecorder?.setAudioSource(MediaRecorder.AudioSource.MIC)
+        mediaRecorder?.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4)
+        mediaRecorder?.setAudioEncoder(MediaRecorder.AudioEncoder.AAC)
+        mediaRecorder?.setOutputFile(output)
     }
 }
